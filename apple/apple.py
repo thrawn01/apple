061cad0cef9ec4c48072395377fd1f937c80daab
Derrick J. Wippler <thrawn01@gmail.com>
2010-07-27 20:57:55 +0000

    Started on async decorators and websocket decorators

--- apple/apple.py
+++ apple/apple.py
@@ -1,61 +1,124 @@
-from bottle import Bottle, request, response, tob
+from bottle import Bottle, request, response, tob, HTTP_CODES, HTTPResponse
 from traceback import format_exc
-import logging from logging
+import logging, sys, pdb, types
+
+import eventlet
+from eventlet import websocket
+from eventlet import semaphore
+from eventlet import wsgi
+from eventlet.green import socket
+
+log = logging.getLogger("apple")
+
+
+class WebSocket( object ):
+    """ Provides a small wrapper for eventlet.WebSocketWSGI """
+
+    def __init__( self, handler ):
+        self.type = 'websocket'
+        self.handler = handler
+        def wrapper( ws ):
+            class Wrapper( object ):
+                def write( self, message ):
+                    return ws.send( message )
+                def read( self ):
+                    return ws.wait( )
+            return handler( Wrapper() )
+        self.websocket = websocket.WebSocketWSGI( wrapper )
+        
+    def __call__(self, environ, start_response):
+        return self.websocket(environ, start_response )
+        
+
+class Async( object ):
+    def __init__( self, handler ):
+        self.type = 'async'
+        self.handler = handler
+
+    def __call__(self, environ, start_response):
+        eventlet_socket = environ['eventlet.input'].get_socket()
+        socket = Socket( eventlet_socket, environ )
+        self.handler( socket )
+        return wsgi.ALREADY_HANDLED
+
+
+class Socket( object ):
+    def __init__( self, socket, environ ):
+        self.socket = socket
+        self.origin = environ.get('HTTP_ORIGIN')
+        self.path = environ.get('PATH_INFO')
+        self.lock = semaphore.Semaphore()
+   
+    def read( self, length ):
+        result = self.socket.recv( length )
+        if result == '':
+            return None
+        return result
+        
+    def write( self, message):
+        self.lock.acquire()
+        try:
+            return self.socket.sendall( message )
+        finally:
+            self.lock.release()
 
 
 class Apple(Bottle):
     """ WSGI application """
+    async = Async
+    websocket = WebSocket
 
     def __init__(self, autojson=True, config=None):
         Bottle.__init__(self, autojson=False, config=config )
-
-        # Setup logging
-        self.log = logging.getlogger("Apple")
-
-
-    def handle(self, url, method):
-        if not self.serve:
-            return HTTPError(503, "Server stopped")
-
-        try:
-            handler, args = self.match_url(url, method)
-            # Call the matching method
-            return handler(**args)
-
-        except HTTPResponse, e:
-            return e
+        self.catchall = False
 
     def __call__(self, environ, start_response):
         """ Apple WSGI-interface. """
 
+        def handle(url, method):
+            handler, args = self.match_url(url, method)
+            log.debug( "Calling: %s(%s)" % (handler.__name__,args) )
+
+            #if isinstance( handler, websocket.WebSocketWSGI ):
+                #log.debug( "Calling '%s' as WebSocketWSGI" % handler.handler.__name__ )
+                #environ['apple.args'] = args
+                #yield handler( environ, start_response )
+            #else:
+                #yield handler(**args)
+
         try:
-            # Tell the WSGI environ about us
             environ['bottle.app'] = self
             request.bind(environ)
             response.bind(self)
-            # Execute the method
-            out = self.handle(request.path, request.method)
-            # Convert the output into somthing wsgi understands
-            out = self._cast(out, request, response)
-            # rfc2616 section 4.3
-            if response.status in (100, 101, 204, 304) or request.method == 'HEAD': out = []
+            result = handle(request.path, request.method)
             status = '%d %s' % (response.status, HTTP_CODES[response.status])
-            # Report the status of the call
+
+            # RFC2616 Section 4.3
+            if response.status in (100, 101, 204, 304) or request.method == 'HEAD':
+                start_response(status, response.headerlist)
+                return []
+
             start_response(status, response.headerlist)
-            # Return the result of the call
-            return out
+            return self._async_cast(result, request, response)
 
         except (KeyboardInterrupt, SystemExit):
             raise
+        except HTTPResponse, e:
+            return e
         except Exception, e:
-            message = "Un-Caught execption '%s' PATH '%s'" % (repr(e), environ.get('PATH_INFO', '/'))
-            # Log the error
-            self.log.error( messge )
+            message = "Un-Caught execption '%s' PATH '%s' " % (repr(e), environ.get('PATH_INFO', '/'))
+            log.error( message )
+            log.error( "%s" % format_exc() )
+
             # Tell the WSGI server about the error
             environ['wsgi.errors'].write( message )
             
             # Let the client know there was an internal error
-            start_response('500 INTERNAL SERVER ERROR', [('Content-Type', 'text/html')])
-            return [tob('Un-Caught exception, Check server logs for stacktrace')]
+            start_response('500 %s' % HTTP_CODES[500], [('Content-Type', 'text/html')])
+            return [tob( '500 %s - Un-Caught exception, Check server logs for stacktrace' % HTTP_CODES[500] ) ]
+
+    def _async_cast(self, result, request, response):
+        for value in result:
+            return self._cast( value, request, response )
 
 
--- apple/test.py
+++ apple/test.py
@@ -1,14 +1,37 @@
 #! /usr/bin/env python
 
-import eventlet
+import eventlet, sys
 from bottle import Bottle, run
+from apple import Apple
+import logging
+import time
 
-wsgi = Bottle()
-wsgi.catchall = False
+#from eventlet import websocket
+#@websocket.WebSocketWSGI
 
-@wsgi.route('/hello')
-def hello():
-    return "From Bottle, Hello World ( My first Web Service )"
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+log = logging.getLogger( "test" )
+
+wsgi = Apple()
+
+@wsgi.route('/websocket')
+@wsgi.websocket
+def websocket(ws):
+    args = ws.environ['apple.args']
+    return "Hello World, with WebSockets"
+
+@wsgi.route('/apple')
+@wsgi.async
+def async( sock ):
+    #result = sock.read()
+    sock.write( "Hello World" )
+
+@wsgi.route('/apple')
+def apple():
+    yield "Wait for it"
+    time.sleep(2)
+    yield "Hello World"
+
 
 if __name__ == "__main__":
     run(app=wsgi, host='thrawn01.org', port=8080)
